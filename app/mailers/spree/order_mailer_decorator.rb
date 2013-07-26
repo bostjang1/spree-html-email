12fb0f36591cdfc0f697a4a562a71da22e7c4b07
Miha <miha.kersic@gmail.com>
2013-07-26 16:24:23 +0200

    lekarar start

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,4 +1,10 @@
 Spree::OrderMailer.class_eval do
   helper 'spree/base'
   layout 'spree/layouts/email'  
+
+  def comment_email(order)
+      find_order(order)
+      subject = "#{Spree::Config[:site_name]} #{t('order_mailer.comment.subject')} ##{@order.number}"
+      mail(:to => @order.email, :from => from_address, :subject => subject)
+  end
 end
