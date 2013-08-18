90f1b91d293246269535edf9f23a3d5a52f2f147
Miha <miha.kersic@gmail.com>
2013-08-18 21:34:28 +0200

    fixes

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,10 +1,11 @@
+# -*- encoding : utf-8 -*-
 Spree::OrderMailer.class_eval do
   helper 'spree/base'
   layout 'spree/layouts/email'  
 
   def comment_email(order)
       find_order(order)
-      subject = "#{Spree::Config[:site_name]} #{t('order_mailer.comment.subject')} ##{@order.number}"
+      subject = "#{Spree::Config[:site_name]} Novo sporočilo v zvezi z naročilom ##{@order.number}"
       mail(:to => @order.email, :from => from_address, :subject => subject)
   end
 end
