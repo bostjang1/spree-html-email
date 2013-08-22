962dad5898aaab186406aefb83e3d173efd081f1
Miha <miha.kersic@gmail.com>
2013-08-22 22:08:42 +0200

    oebni prevzem

--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 Spree::ShipmentMailer.class_eval do
   helper 'spree/base'
   layout 'spree/layouts/email'
--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 Spree::UserMailer.class_eval do
   helper 'spree/base'
   layout 'spree/layouts/email'
